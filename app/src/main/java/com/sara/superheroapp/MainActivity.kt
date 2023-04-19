41b0b5ddbf664228b8f4df0742bc4729a0720f8a
Sara <saraying03sb@gmail.com>
2023-04-19 16:12:24 +0200

    final

--- app/src/main/java/com/sara/superheroapp/MainActivity.kt
+++ app/src/main/java/com/sara/superheroapp/MainActivity.kt
@@ -1,90 +1,12 @@
 package com.sara.superheroapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.widget.SearchView
-import androidx.core.view.isVisible
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.sara.superheroapp.DetailSuperheroActivity.Companion.EXTRA_ID
-import com.sara.superheroapp.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var  binding: ActivityMainBinding
-    private lateinit var retrofit: Retrofit
-    private lateinit var adapter: SuperheroAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        retrofit=getRetrofit()
-        initUi()
+        setContentView(R.layout.activity_main)
     }
 
-    private fun initUi() {
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener
-        {
-            override fun onQueryTextSubmit(query: String?): Boolean {//Cuando pulsamos en buscar, la lupa
-
-                searchByName(query.orEmpty())
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {//Se llama a medida que vamos escribiendo
-                return false
-            }
-        }
-        )
-
-        adapter = SuperheroAdapter{navigateToDetail(it)}
-        binding.rvSuperhero.setHasFixedSize(true)
-        binding.rvSuperhero.layoutManager = LinearLayoutManager(this)
-        binding.rvSuperhero.adapter = adapter
-    }
-
-    private fun searchByName(query: String) {
-        binding.progressBar.isVisible = true
-
-        CoroutineScope(Dispatchers.IO).launch {
-            val myResponse: Response<SuperheroDataResponse> = retrofit.create(ApiService::class.java).getSuperheroes(query)
-            if(myResponse.isSuccessful) {
-                val response: SuperheroDataResponse? = myResponse.body()
-                if(response != null){
-                    Log.i("Sara", response.toString())
-                    runOnUiThread{
-                        adapter.updateList(response.superheroes)
-                        binding.progressBar.isVisible = false
-                    }
-                }
-
-            }
-            else Log.i("Sara", "No funciona")
-
-        }
-        binding.progressBar.isVisible = false
-    }
-
-    private fun getRetrofit(): Retrofit{
-        return Retrofit
-            .Builder()
-            .baseUrl("https://superheroapi.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
-
-    private fun navigateToDetail(id:String){
-        val intent = Intent(this, DetailSuperheroActivity::class.java)
-        intent.putExtra(EXTRA_ID,id)
-        startActivity(intent)
-
-    }
 }
